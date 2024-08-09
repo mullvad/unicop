4e644ab09536cd63d8bfa82ce8a4e4cf9665198b
Grégoire Détrez <gregoire@mullvad.net>
2024-08-09 17:26:42 +0200

    WIP: gen names table

---
+++ examples/allow.py
@@ -0,0 +1,8 @@
+# Unicode allowed comments
+# å
+
+# Unicode allowed in strings
+"å", """å""", "å", """å"""
+r"å", r"""å""", r"å", r"""å"""
+"å", """å""", "å", """å"""
+f"å", f"""å""", f"å", f"""å"""
