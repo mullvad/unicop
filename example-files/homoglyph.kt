018908c950dcd682770d2cd4bfd90ade9fd9a62a
Grégoire Détrez <gregoire@mullvad.net>
2024-09-09 15:26:42 +0200

    WIP: Kotlin

---
+++ example-files/homoglyph.kt
@@ -0,0 +1,14 @@
+// Allowed in commentsǃ
+/* Allowed in 
+   multiline commentsǃ */
+
+fun main(args : Array<String>) {
+    if environmentǃ="PROD" {
+        // ...
+    }
+    println("Hello, World!")
+    print("Allowed in stringǃ") 
+    print("""
+      Allowed in multiline stringǃ
+    """) 
+}
